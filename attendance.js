268232e13026dad1187895f6a75d40517757f19c
hdsft <hatairy.dossantos@ft.com>
2021-06-24 10:17:31 +0100

    show occupancy

--- attendance.js
+++ attendance.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const schema = new mongoose.Schema({ date: 'string', occupancy: 'string' });
+const schema = new mongoose.Schema({ date: String, occupancy: Number });
 const Attendance = mongoose.model('Attendance', schema);
 
 module.exports = Attendance
---
+++ seeddata.js
@@ -0,0 +1,21 @@
+const Attendance = require("./attendance.js")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/capdata', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connection succesfull")
+});
+
+
+Attendance.insertMany([
+    {date: '21/06/2021', occupancy: 250},
+    {date: '22/06/2021', occupancy: 400},
+    {date: '23/06/2021', occupancy: 150},
+    {date: '24/06/2021', occupancy: 250},
+    {date: '25/06/2021', occupancy: 50}
+]).then(function(){
+    console.log("data inserted")
+}).catch(function(error) {
+    console.log(error) 
+});
