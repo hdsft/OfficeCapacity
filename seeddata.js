4cfafb6054d4b6b210d62b0e77a2ad1781f316b4
hdsft <hatairy.dossantos@ft.com>
2021-07-14 14:21:02 +0100

    chart

--- seeddata.js
+++ seeddata.js
@@ -13,7 +13,7 @@ Attendance.insertMany([
     {date: '22/06/2021', occupancy: 400},
     {date: '23/06/2021', occupancy: 150},
     {date: '24/06/2021', occupancy: 250},
-    {date: '25/06/2021', occupancy: 50}
+    {date: '25/06/2021', occupancy: 50000}
 ]).then(function(){
     console.log("data inserted")
 }).catch(function(error) {
